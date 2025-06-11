3344c679efa45c7cae44b73b877290c4f906dad8
M073mmad <dmhnard@gmail.com>
2025-06-11 15:06:40 +0300

    Update auth.php

--- htdocs/auth.php
+++ htdocs/auth.php
@@ -4,9 +4,9 @@ require_once 'vendor/autoload.php';
 session_start();
 
 $client = new Google_Client();
-$client->setClientId('YOUR_CLIENT_ID');
-$client->setClientSecret('YOUR_CLIENT_SECRET');
-$client->setRedirectUri('https://example.com/oauth2callback.php');
+$client->setClientId('632728965271-reiv5re5tbuu4k3l9npt33n9ih4hbtho.apps.googleusercontent.com');
+$client->setClientSecret('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');
+$client->setRedirectUri('https://fro5.onrender.com/oauth2callback.php');
 $client->addScope(Google_Service_Drive::DRIVE_READONLY);
 
 if (!isset($_GET['code'])) {
