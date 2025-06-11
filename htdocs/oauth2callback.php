1b967dc0a2c4c272d45a7d1c92483d6fbfaf85e0
M073mmad <dmhnard@gmail.com>
2025-06-11 14:25:29 +0300

    Update oauth2callback.php

--- htdocs/oauth2callback.php
+++ htdocs/oauth2callback.php
@@ -5,7 +5,7 @@ session_start();
 $client = new Google_Client();
 $client->setClientId('632728965271-reiv5re5tbuu4k3l9npt33n9ih4hbtho.apps.googleusercontent.com');
 $client->setClientSecret('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');
-$client->setRedirectUri('http://fro5.ct.ws/oauth2callback.php');
+$client->setRedirectUri('https://fro5.onrender.com/oauth2callback.php');
 $client->addScope(Google_Service_Drive::DRIVE_READONLY);
 
 if (isset($_GET['code'])) {
