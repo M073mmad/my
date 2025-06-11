6174247069009e301ccac1e0367bdb859b3376f8
M073mmad <dmhnard@gmail.com>
2025-06-11 15:59:58 +0300

    Update gallery.php

--- htdocs/gallery.php
+++ htdocs/gallery.php
@@ -4,8 +4,8 @@ require_once(__DIR__ . '/vendor/autoload.php');
 session_start();
 
 $client = new Google_Client();
-$client->setClientId('632728965271-reiv5re5tbuu4k3l9npt33n9ih4hbtho.apps.googleusercontent.com');
-$client->setClientSecret('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');
+$client->setClientId('733626945827-b6ae5591pdi6itku1u0mhm6a926hsni0.apps.googleusercontent.com');
+$client->setClientSecret('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');
 $client->setRedirectUri('https://fro5.onrender.com/oauth2callback.php');
 $client->addScope(Google_Service_Drive::DRIVE_READONLY);
 
