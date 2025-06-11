f6e902fec3b70b5ecd6e72858926d46486e544b3
M073mmad <dmhnard@gmail.com>
2025-06-12 00:52:03 +0300

    Update oauth2callback.php

--- htdocs/oauth2callback.php
+++ htdocs/oauth2callback.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // تحميل مكتبة Google API من المجلد الأعلى (vendor)
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php'; // ✅ يشير للمسار الصحيح
 
 session_start();
 
