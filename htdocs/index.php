23a050e0cc4d6bccb92d286fb18c2659511a5669
M073mmad <dmhnard@gmail.com>
2025-06-11 13:32:51 +0300

    Update index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 
-$usersFile = __DIR__ . '/../secure/users.txt';
+$usersFile = __DIR__ . '/secure/users.txt';
+
 $error = '';
 
 if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
