4b9d0b3492f7c4ec114c5c04821d325b23618867
M073mmad <dmhnard@gmail.com>
2025-06-12 16:40:25 +0300

    Update videos.php

--- htdocs/videos.php
+++ htdocs/videos.php
@@ -133,6 +133,18 @@ foreach ($results->getFiles() as $file) {
       text-decoration: none;
       display: inline-block;
     }
+      .btn3 {
+      padding: 10px 20px;
+      background: pink;
+      color: white;
+      border: none;
+      border-radius: 6px;
+      cursor: pointer;
+      font-size: 16px;
+      margin-bottom: 20px;
+      text-decoration: none;
+      display: inline-block;
+    }
   </style>
 </head>
 <body>
@@ -142,6 +154,7 @@ foreach ($results->getFiles() as $file) {
     <a href="gallery.php" class="btn">العودة</a>
     <a href="videostrok.php" class="btn1">تروك</a>
     <a href="videostim.php" class="btn2">تمساح</a>
+    <a href="videoshmod.php" class="btn3">حمود</a>
   </div>
 
   <div class="gallery">
