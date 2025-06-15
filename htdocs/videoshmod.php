49a4629afa112634e37ebf41fee4f5e1e4c511ff
M073mmad <dmhnard@gmail.com>
2025-06-15 14:21:46 +0300

    Update videoshmod.php

--- htdocs/videoshmod.php
+++ htdocs/videoshmod.php
@@ -126,7 +126,7 @@ foreach ($results->getFiles() as $file) {
 
   <h2>معرض حمود</h2>
 
-    <a href="videos.php" class="back-btn">←العودة</a>
+    <a href="arb.html" class="back-btn">←العودة</a>
 
   <div class="gallery">
     <?php foreach ($videos as $video): ?>
