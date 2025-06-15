cd47935519e540af51fd0233954de81d777302ad
M073mmad <dmhnard@gmail.com>
2025-06-15 16:31:43 +0300

    Update auth.php

--- htdocs/auth.php
+++ htdocs/auth.php
@@ -6,7 +6,7 @@ session_start();
 $client = new Google_Client();
 $client->setClientId('733626945827-b6ae5591pdi6itku1u0mhm6a926hsni0.apps.googleusercontent.com');
 $client->setClientSecret('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');
-$client->setRedirectUri('https://fro5.onrender.com/oauth2callback.php');
+$client->setRedirectUri('https://distant-marin-m073mmad-7f9e1fee.koyeb.app/oauth2callback.php');
 $client->addScope(Google_Service_Drive::DRIVE_READONLY);
 
 if (!isset($_GET['code'])) {
