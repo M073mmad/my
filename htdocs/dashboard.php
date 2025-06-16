b808a7ef76cc991718ba17b0d2fc2fc37a1582f9
M073mmad <dmhnard@gmail.com>
2025-06-16 15:57:05 +0300

    Update dashboard.php

--- htdocs/dashboard.php
+++ htdocs/dashboard.php
@@ -67,7 +67,7 @@ if (!isset($_SESSION['access_token'])) {
 <div class="dashboard">
   <h1>مرحباً، <?php echo htmlspecialchars($_SESSION['user']); ?>!</h1>
   <a href="2agn.php" class="btn">الأجانب</a>
-  <a href="2arb.php" class="btn">المعرض العام</a>
+  <a href="arb.php" class="btn">المعرض العام</a>
   <a href="shf.php" class="btn">معرض الشفايف</a>
 
   <a href="logout.php" class="btn" style="background: #dc3545;">تسجيل الخروج</a>
