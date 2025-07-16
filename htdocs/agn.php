26f9d61f9fc51adae59ff83dc0772d3122db9bd0
M073mmad <dmhnard@gmail.com>
2025-07-17 02:10:46 +0300

    Update agn.php

--- htdocs/agn.php
+++ htdocs/agn.php
@@ -116,24 +116,35 @@ if ($client->isAccessTokenExpired()) {
 $service = new Google_Service_Drive($client);
 
 $folderId = "1dEnJm3uCFx272CQyHKK4ptSjjIdPZre8";
-$results = $service->files->listFiles([
-    'q' => "'$folderId' in parents and trashed = false",
-    'fields' => 'files(id,name,mimeType,thumbnailLink)'
-]);
+$optParams = [
+    'q'        => "'$folderId' in parents and trashed = false and mimeType contains 'image/'",
+    'fields'   => 'nextPageToken, files(id,name,mimeType,thumbnailLink)',
+    'pageSize' => 1000,
+];
 
 $images = [];
+$pageToken = null;
 $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif', 'webp', 'bmp', 'svg'];
-foreach ($results->getFiles() as $file) {
-    if (strpos($file->getMimeType(), 'image/') === 0) {
+
+do {
+    if ($pageToken) {
+        $optParams['pageToken'] = $pageToken;
+    }
+
+    $results = $service->files->listFiles($optParams);
+
+    foreach ($results->getFiles() as $file) {
         $ext = strtolower(pathinfo($file->getName(), PATHINFO_EXTENSION));
         if (in_array($ext, $allowedExtensions)) {
             $images[] = [
                 'thumb' => $file->getThumbnailLink(),
-                'id' => $file->getId(),
+                'id'    => $file->getId(),
             ];
         }
     }
-}
+
+    $pageToken = $results->getNextPageToken();
+} while ($pageToken);
 ?>
 
 <!DOCTYPE html>
